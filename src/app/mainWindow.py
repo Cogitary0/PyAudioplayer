e3f76087455cb4b912fc72d4abeb9d0e6e3bc221
Cogi Asd <asadovserega2@gmail.com>
2024-06-28 19:20:46 +0300

    "Refactored MainWindow class: extracted window dimensions to constants, added icons for play/stop button, merged play and stop buttons into one, and updated UI initialization and event handling logic."

--- src/app/mainWindow.py
+++ src/app/mainWindow.py
@@ -2,15 +2,17 @@ import sys
 from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
 from PyQt5.QtCore import QUrl
-from PyQt5.QtGui import QColor, QPalette
+from PyQt5.QtGui import QColor, QPalette, QIcon
+
+WINW = 360
+WINH = 140
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
-        self.WINW = 360
-        self.WINH = 140
-        self.setGeometry(300, 300, self.WINW, self.WINH)
-        self.setFixedSize(self.WINW, self.WINH)
+
+        self.setGeometry(300, 300, WINW, WINH)
+        self.setFixedSize(WINW, WINH)
         self.setWindowTitle('Music Player')
         self.initUI()
         
@@ -19,7 +21,13 @@ class MainWindow(QWidget):
         
         self.media_player = QMediaPlayer(None, QMediaPlayer.StreamPlayback)
         self.media_player.mediaStatusChanged.connect(self.printMediaData)
+        
         self.filename = None
+        self.playing = False
+        self.play_icon = QIcon('src\\assets\\icons\\play.png')
+        self.stop_icon = QIcon('src\\assets\\icons\\stop.png')
+
+        
 
     def initUI(self):
         layout = QVBoxLayout()
@@ -30,41 +38,49 @@ class MainWindow(QWidget):
         self.openFileButton = QPushButton('Open File')
         self.openFileButton.clicked.connect(self.open_file)
         layout.addWidget(self.openFileButton)
-
-        self.playButton = QPushButton('Play')
-        self.playButton.clicked.connect(self.play_song)
-        self.playButton.setEnabled(False)
-        layout.addWidget(self.playButton)
-
-        self.stopButton = QPushButton('Stop')
-        self.stopButton.clicked.connect(self.stop_song)
-        self.stopButton.setEnabled(False)
-        layout.addWidget(self.stopButton)
+        
+        self.playStopButton = QPushButton("")
+        self.playStopButton.clicked.connect(self.play_stop_song)
+        self.playStopButton.setEnabled(False)
+        layout.addWidget(self.playStopButton)
+        
 
         self.setLayout(layout)
 
+
     def printLabel(self, text) -> None:
         self.statusLabel.setText(text)
+
 
     def open_file(self):
         self.filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Audio Files (*.mp3 *.wav)")
         if self.filename:
             self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
-            self.playButton.setEnabled(True)
-            self.stopButton.setEnabled(True)
+            self.playStopButton.setEnabled(True)
+            self.playStopButton.setIcon(self.play_icon)
 
-    def play_song(self):
+    def play_stop_song(self):
         if self.filename:
-            self.media_player.play()
+            if not self.playing:
+                self.media_player.play()
 
-    def stop_song(self):
-        if self.filename:
-            self.media_player.stop()
+                self.playStopButton.setIcon(self.stop_icon)
+                self.playing = True
+            else:
+                self.media_player.stop()
+
+                self.playStopButton.setIcon(self.play_icon)
+                self.playing = False
+
 
     def printMediaData(self):
         if self.media_player.mediaStatus() == 6:
             if self.media_player.isMetaDataAvailable():
-                res = str(self.media_player.metaData("Resolution")).partition("PyQt5.QtCore.QSize(").replace(", ", "x").replace(")", "")
-                print("%s%s" % ("Audio Resolution = ", res))
+                title = self.media_player.metaData("Title")
+                author = self.media_player.metaData("Author")
+                
+                self.printLabel("{} {}".format(title,author))
+                
+                print(f"Title: {title}, Author: {author}")
             else:
                 print("no metaData available")
