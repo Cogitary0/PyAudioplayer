4d84d2ed734980af1db535a2924a69cd05144665
Cogi Asd <asadovserega2@gmail.com>
2024-06-28 20:52:05 +0300

    "Updated MainWindow.py to add folder selection, music file listing, and playback controls; modified styles.css to add slider styling; removed leftArrow.png and rightArrow.png icons."

--- src/app/mainWindow.py
+++ src/app/mainWindow.py
@@ -1,20 +1,19 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel
+import os
+from PyQt5.QtWidgets import QApplication, QWidget, QVBoxLayout, QPushButton, QFileDialog, QLabel, QSlider
 from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
-from PyQt5.QtCore import QUrl
+from PyQt5.QtCore import QUrl, QCoreApplication, Qt, QSize
 from PyQt5.QtGui import QColor, QPalette, QIcon
 
-WINW = 360
-WINH = 140
+WINW = 460
+WINH = 340
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
 
-        self.setGeometry(300, 300, WINW, WINH)
-        self.setFixedSize(WINW, WINH)
         self.setWindowTitle('Music Player')
-        self.initUI()
+        self.setFixedSize(QSize(WINW, WINH))
         
         with open("src\\assets\\css\\styles.css", 'r') as styleFile:
             self.setStyleSheet(styleFile.read())
@@ -22,29 +21,59 @@ class MainWindow(QWidget):
         self.media_player = QMediaPlayer(None, QMediaPlayer.StreamPlayback)
         self.media_player.mediaStatusChanged.connect(self.printMediaData)
         
-        self.filename = None
+        self.folder_path = None
+        self.music_files = []
+        self.current_song = 0
         self.playing = False
+        
         self.play_icon = QIcon('src\\assets\\icons\\play.png')
         self.stop_icon = QIcon('src\\assets\\icons\\stop.png')
+        self.next_icon = QIcon('src\\assets\\icons\\next.png')
+        self.prev_icon = QIcon('src\\assets\\icons\\prev.png')
 
-        
+        self.initUI()
 
     def initUI(self):
         layout = QVBoxLayout()
         
         self.statusLabel = QLabel()
-        layout.addWidget(self.statusLabel)
+       
 
-        self.openFileButton = QPushButton('Open File')
-        self.openFileButton.clicked.connect(self.open_file)
-        layout.addWidget(self.openFileButton)
+        self.openFolderButton = QPushButton('Open Folder')
+        self.openFolderButton.clicked.connect(self.open_folder)
+        
         
         self.playStopButton = QPushButton("")
+        self.playStopButton.setIcon(self.play_icon)
         self.playStopButton.clicked.connect(self.play_stop_song)
         self.playStopButton.setEnabled(False)
-        layout.addWidget(self.playStopButton)
         
 
+        self.prevButton = QPushButton("")
+        self.prevButton.setIcon(self.prev_icon)
+        self.prevButton.clicked.connect(self.prev_song)
+        self.prevButton.setEnabled(False)
+        
+
+        self.nextButton = QPushButton("")
+        self.nextButton.setIcon(self.next_icon)
+        self.nextButton.clicked.connect(self.next_song)
+        self.nextButton.setEnabled(False)
+        
+
+        self.volumeSlider = QSlider(orientation=Qt.Horizontal)
+        self.volumeSlider.setRange(0, 100)
+        self.volumeSlider.setValue(50)
+        self.volumeSlider.valueChanged.connect(self.change_volume)
+        
+        
+        
+        layout.addWidget(self.statusLabel)
+        layout.addWidget(self.openFolderButton)
+        layout.addWidget(self.playStopButton)
+        layout.addWidget(self.prevButton)
+        layout.addWidget(self.nextButton)
+        layout.addWidget(self.volumeSlider)
         self.setLayout(layout)
 
 
@@ -52,25 +81,54 @@ class MainWindow(QWidget):
         self.statusLabel.setText(text)
 
 
-    def open_file(self):
-        self.filename, _ = QFileDialog.getOpenFileName(self, "Open File", "", "Audio Files (*.mp3 *.wav)")
-        if self.filename:
-            self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(self.filename)))
-            self.playStopButton.setEnabled(True)
-            self.playStopButton.setIcon(self.play_icon)
+    def open_folder(self):
+        self.folder_path = QFileDialog.getExistingDirectory(self, "Open Folder")
+        if self.folder_path:
+            self.music_files = [f for f in os.listdir(self.folder_path) if f.endswith('.mp3') or f.endswith('.wav')]
+            if self.music_files:
+                self.playStopButton.setEnabled(True)
+                self.prevButton.setEnabled(True)
+                self.nextButton.setEnabled(True)
+
+            else:
+                self.printLabel("No music files found in the folder.")
+
+
+    def play_song(self, filename):
+        self.media_player.setMedia(QMediaContent(QUrl.fromLocalFile(os.path.join(self.folder_path, filename))))
+        self.media_player.play()
+        self.playStopButton.setIcon(self.stop_icon)
+        self.playing = True
+
 
     def play_stop_song(self):
-        if self.filename:
-            if not self.playing:
-                self.media_player.play()
+        if self.playing:
+            self.media_player.stop()
+            self.playStopButton.setIcon(self.play_icon)
+            self.playing = False
+        else:
+            self.media_player.play()
+            self.playStopButton.setIcon(self.stop_icon)
+            self.playing = True
 
-                self.playStopButton.setIcon(self.stop_icon)
-                self.playing = True
-            else:
-                self.media_player.stop()
 
-                self.playStopButton.setIcon(self.play_icon)
-                self.playing = False
+    def prev_song(self):
+        self.current_song = self.current_song if self.current_song > 0 else len(self.music_files)
+        self.media_player.stop()
+        self.play_song(self.music_files[self.current_song - 1])
+        self.current_song -= 1
+        
+
+
+    def next_song(self):
+        self.current_song = self.current_song if self.current_song < len(self.music_files) else 0
+        self.media_player.stop()
+        self.play_song(self.music_files[self.current_song + 1])
+        self.current_song += 1
+
+
+    def change_volume(self, value):
+        self.media_player.setVolume(value)
 
 
     def printMediaData(self):
