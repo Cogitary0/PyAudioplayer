021d89d878db1e1402c014fbaac2b417d05ffd2e
Cogi Asd <asadovserega2@gmail.com>
2024-07-12 21:14:20 +0300

    ref mainwindow, volumewindow

--- src/app/mainWindow.py
+++ src/app/mainWindow.py
@@ -2,17 +2,20 @@ import os
 import time
 import sys
 from keyboard import add_hotkey
-from PyQt5.QtCore import QSize, QTimer
-from PyQt5.QtGui import QIcon, QPixmap, QPalette, QBrush
-from PyQt5.QtMultimedia import QMediaPlayer
+
 from src.app.musicPlayback import MediaPlaybackThread
 from src.app.settingsWindow import SettingsWindow
 from src.app.downloaderWindow import DownloaderWindow
+from src.app.volumeWindow import VolumeWindow
 from src.utils.parse import Settings, Language
+
+from PyQt5.QtCore import QSize, QTimer
+from PyQt5.QtGui import QIcon, QPixmap, QPalette, QBrush
+from PyQt5.QtMultimedia import QMediaPlayer
 from PyQt5.QtWidgets import (QWidget,
                              QLabel, 
                              QVBoxLayout, 
-                             QPushButton, 
+                             QPushButton,
                              QFileDialog, 
                              QProgressBar,
                              QHBoxLayout)
@@ -30,38 +33,18 @@ class MainWindow(QWidget):
         
         self.settings = Settings(configPath)
         self.lg = Language(langPath, self.settings.get('language')).get
-
-        WIN_WIDTH, WIN_HEIGHT = self.settings.get('win_width'), self.settings.get('win_height')
         
-        self.play_icon = QIcon(ICONS_PATH + 'play.png')
-        self.stop_icon = QIcon(ICONS_PATH + 'pause.png')
-        self.next_icon = QIcon(ICONS_PATH + 'next.png')
-        self.prev_icon = QIcon(ICONS_PATH + 'prev.png')
-        self.folder_icon = QIcon(ICONS_PATH + 'folder_filled.png')
-        self.downloader_icon = QIcon(ICONS_PATH + 'downloader.png')
-        self.settings_icon = QIcon(ICONS_PATH + 'settings.png')
-        self.setStyleSheet(self.get_style_file('styles'))
-        self.setFixedSize(QSize(WIN_WIDTH, WIN_HEIGHT))
-        
-        bgnd = QPixmap(BACKGROUND_PATH).scaled(WIN_WIDTH, WIN_HEIGHT)
-        # bgnd = QPixmap("C:\\Users\\asd\\Desktop\\Kraject\\AppPython\\PyPyAudioplayer\\src\\app\\assets\\icons\\main_background.png").scaled(400,100)
-        pal = self.palette()
-        pal.setBrush(QPalette.Background, QBrush(bgnd))
-        self.setPalette(pal)
-        
-        self.media_player = QMediaPlayer(None, QMediaPlayer.StreamPlayback)
-        self.media_player.mediaStatusChanged.connect(self.print_media_data)
-        self.media_player.positionChanged.connect(self.update_position)
-        self.media_player.durationChanged.connect(self.update_duration)
-        self.media_player.stateChanged.connect(self.media_state_changed)
-
         self.folder_path = self.settings.get('path_to_music')
         self.current_song = self.settings.get('current_song')
+        
         self.music_duration = 0
         self.playing = False
         self.music_files = []
         self.audio_trigger = True
 
+        self.init_background()
+        self.init_media()
+        self.init_assets()
         self.init_ui()
 
         # No music folder
@@ -70,44 +53,70 @@ class MainWindow(QWidget):
         else:
             self.print_label(f" {self.lg('NoMeta')}")
 
-    
+
+    def init_background(self):
+        WIN_WIDTH, WIN_HEIGHT = self.settings.get('win_width'), self.settings.get('win_height')
+        self.setFixedSize(QSize(WIN_WIDTH, WIN_HEIGHT))
+        
+        background_img = QPixmap(BACKGROUND_PATH).scaled(WIN_WIDTH, WIN_HEIGHT)
+        pal = self.palette()
+        pal.setBrush(QPalette.Background, QBrush(background_img))
+        self.setPalette(pal)
+
+
+    def init_media(self):
+        self.media_player = QMediaPlayer(None, QMediaPlayer.StreamPlayback)
+        self.media_player.mediaStatusChanged.connect(self.print_media_data)
+        self.media_player.positionChanged.connect(self.update_position)
+        self.media_player.durationChanged.connect(self.update_duration)
+        self.media_player.stateChanged.connect(self.media_state_changed)
+
+
+    def init_assets(self):
+        self.menu_icon = QIcon(ICONS_PATH + 'menu.png')
+        self.play_icon = QIcon(ICONS_PATH + 'play.png')
+        self.stop_icon = QIcon(ICONS_PATH + 'pause.png')
+        self.next_icon = QIcon(ICONS_PATH + 'next.png')
+        self.prev_icon = QIcon(ICONS_PATH + 'prev.png')
+        self.folder_icon = QIcon(ICONS_PATH + 'folder_filled.png')
+        self.downloader_icon = QIcon(ICONS_PATH + 'downloader.png')
+        self.settings_icon = QIcon(ICONS_PATH + 'settings.png')
+        self.volume_on_icon = QIcon(ICONS_PATH + 'vol_on.png')
+        self.volume_off_icon = QIcon(ICONS_PATH + 'vol_off.png')
+        self.setStyleSheet(self.get_style_file('styles'))
+        
 
     def init_ui(self):
         layout = QVBoxLayout()
         controlLayout = QHBoxLayout()
-        sliderLayout = QHBoxLayout()
-
-        # self.statusLabel = QLabel()
-        # self.statusLabel.setFixedHeight(60)
-        # layout.addWidget(self.statusLabel)
-        
-        # self.volumeProgressBar = QProgressBar()
-        # self.volumeProgressBar.setRange(0, 100)
-        # self.volumeProgressBar.setValue(self.settings.get('def_volume'))
-        # self.volumeProgressBar.setTextVisible(False)
-        # self.volumeProgressBar.setFixedSize(377, 8)
-        # self.volume_up_shortcut = add_hotkey(self.settings.get('btn_volume_up'), self.volume_up)
-        # self.volume_down_shortcut = add_hotkey(self.settings.get('btn_volume_down'), self.volume_down)
-        # sliderLayout.addWidget(self.volumeProgressBar, stretch=1)
         
         self.positionProgressBar = QProgressBar()
         self.positionProgressBar.setRange(0, 0)
         self.positionProgressBar.setValue(0)
         self.positionProgressBar.setTextVisible(False)
-        self.positionProgressBar.setFixedSize(370,6)
+        self.positionProgressBar.setFixedHeight(7)
         self.position_plus_shortcut = add_hotkey(self.settings.get('btn_music_plus'), self.position_plus)
         self.position_plus_shortcut = add_hotkey(self.settings.get('btn_music_minus'), self.position_minus)
-        sliderLayout.addWidget(self.positionProgressBar)
+        layout.addWidget(self.positionProgressBar, stretch = 3)
+
+        # self.volumeProgressBar = QProgressBar()
+        # self.volumeProgressBar.setRange(0, 100)
+        # self.volumeProgressBar.setValue(self.settings.get('def_volume'))
+        # self.volumeProgressBar.setTextVisible(False)
+        # self.volumeProgressBar.setFixedHeight(7)
+        # self.volume_up_shortcut = add_hotkey(self.settings.get('btn_volume_up'), self.volume_up)
+        # self.volume_down_shortcut = add_hotkey(self.settings.get('btn_volume_down'), self.volume_down)
+        # layout.addWidget(self.volumeProgressBar, stretch = 1)
 
         # self.settingsButton = QPushButton()
         # self.settingsButton.setIcon(self.settings_icon)
         # self.settingsButton.clicked.connect(self.open_settings)
         # controlLayout.addWidget(self.settingsButton, stretch=1)
 
-        self.openFolderButton = QPushButton()
-        self.openFolderButton.setIcon(self.folder_icon)
-        self.openFolderButton.clicked.connect(self.open_folder)
-        controlLayout.addWidget(self.openFolderButton, stretch=1)
+        self.volumeWindowButton = QPushButton()
+        self.volumeWindowButton.setIcon(self.volume_on_icon)
+        self.volumeWindowButton.clicked.connect(self.open_volume)
+        controlLayout.addWidget(self.volumeWindowButton)
 
         self.prevButton = QPushButton()
         self.prevButton.setIcon(self.prev_icon)
@@ -123,13 +132,17 @@ class MainWindow(QWidget):
         self.nextButton.setIcon(self.next_icon)
         self.nextButton.clicked.connect(self.next_song)
         controlLayout.addWidget(self.nextButton, stretch=1)
-
-        self.windowDownloader = QPushButton()
-        self.windowDownloader.setIcon(self.downloader_icon)
-        self.windowDownloader.clicked.connect(self.open_downloader)
-        controlLayout.addWidget(self.windowDownloader, stretch=1)
-
-        layout.addLayout(sliderLayout)
+        
+        self.openFolderButton = QPushButton()
+        self.openFolderButton.setIcon(self.folder_icon)
+        self.openFolderButton.clicked.connect(self.open_folder)
+        controlLayout.addWidget(self.openFolderButton, stretch=1)
+        
+        # self.menuButton = QPushButton()
+        # self.menuButton.setIcon(self.menu_icon)
+        # self.menuButton.clicked.connect(self.open_menu)
+        # controlLayout.addWidget(self.menuButton, stretch=1)
+        
         layout.addLayout(controlLayout)
 
         self.setLayout(layout)
@@ -159,12 +172,6 @@ class MainWindow(QWidget):
             self.prevButton.setEnabled(enabled)
             self.nextButton.setEnabled(enabled)
             self.positionProgressBar.setEnabled(enabled)
-
-
-    def open_folder(self):
-        self.folder_path = QFileDialog.getExistingDirectory(self, "Open Folder")
-        if self.folder_path:
-            self.set_music()
             
             
     def set_music(self):
@@ -184,7 +191,7 @@ class MainWindow(QWidget):
         if self.playing:
             self.media_player.stop()
             
-        self.media_player.setVolume(self.volumeProgressBar.value())
+        # self.media_player.setVolume(self.volumeProgressBar.value())
         
         thread = MediaPlaybackThread(self.media_player, 
                                      self.folder_path, 
@@ -303,3 +310,14 @@ class MainWindow(QWidget):
         self.downloader_window = DownloaderWindow(self.get_style_file('styles'), self.folder_path)
         self.downloader_window.destroyed.connect(self.set_music)
         self.downloader_window.show()
+        
+    
+    def open_folder(self):
+        self.folder_path = QFileDialog.getExistingDirectory(self, "Open Folder")
+        if self.folder_path:
+            self.set_music()
+            
+        
+    def open_volume(self):
+        self.volume_window = VolumeWindow(self.settings, self.get_style_file('settings'))
+        
--- src/app/volumeWindow.py
+++ src/app/volumeWindow.py
@@ -1,16 +1,33 @@
-from PyQt5.QtGui import QIcon
-from PyQt5.QtWidgets import (QWidget)
 from src.utils.parse import Settings
 
+from PyQt5.QtCore import Qt
+from PyQt5.QtGui import QIcon
+from PyQt5.QtWidgets import (QWidget, 
+                             QHBoxLayout, 
+                             QPushButton, 
+                             QSlider,
+                             QLineEdit)
+
+
+
+
 class VolumeWindow(QWidget):
-    def __init__(self, configPath, style):
+    def __init__(self, settings, style):
         super().__init__()
         
-        self.settings = Settings(configPath)
+        self.settings = settings
         
+        self.setWindowFlag(Qt.FramelessWindowHint)
         self.setStyleSheet(style)
+        self.setFixedSize(30,10)
+        
         self.init_ui()
     
     def init_ui(self):
-        ...
+        __layout = QHBoxLayout()
+        
+        self.volumeSlide = QSlider()
+        
+        self.setLayout(__layout)
+
         
